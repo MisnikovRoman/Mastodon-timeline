7a560db4e27ce3d1fef0fe824918c2ca58df9669
Roman <misnikovroman@gmail.com>
2018-04-03 10:48:22 +0300

    add VCs

---
+++ Mastodon-timeline/Controller/DetailsVC.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsVC.swift
+//  Mastodon-timeline
+//
+//  Created by Роман Мисников on 03.04.2018.
+//  Copyright © 2018 Роман Мисников. All rights reserved.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ Mastodon-timeline/Controller/TimelineVC.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineVC.swift
+//  Mastodon-timeline
+//
+//  Created by Роман Мисников on 03.04.2018.
+//  Copyright © 2018 Роман Мисников. All rights reserved.
+//
+
+import UIKit
+
+class TimelineVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
